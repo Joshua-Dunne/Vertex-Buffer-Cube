ced2e3ace57afee77ea5b817e7a321748ac1e9c9
Joshua Dunne <C00241588@itcarlow.ie>
2020-01-21 10:17:03 +0000

    Fixed Translate function in Matrix3. Removed unused vertices. Added more triangles with anti-clockwise winding, to help with drawing the cube. Made the cube draw.

--- SFMLOpenGL/Main.cpp
+++ SFMLOpenGL/Main.cpp
@@ -1,3 +1,29 @@
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-main-d.lib")
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-main.lib")
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+#pragma comment (lib, "OpenGL32.lib")
+#pragma comment (lib, "GlU32.lib")
+#pragma comment (lib, "glew32.lib")
+#pragma comment (lib, "glew32s.lib")  
+
+/// <summary>
+/// Joshua Dunne
+/// C00241588
+/// </summary>
+
+
 #include <Game.h>
 
 int main(void)
